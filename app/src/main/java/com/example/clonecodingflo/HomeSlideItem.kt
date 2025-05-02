545a905fe09ba34387ca9c6af5033b4c6c0eb34a
sxunxin <whang31658718@gmail.com>
2025-05-03 00:31:16 +0900

    week5

---
+++ app/src/main/java/com/example/clonecodingflo/HomeSlideItem.kt
@@ -0,0 +1,13 @@
+package com.example.clonecodingflo
+
+data class HomeSlideItem(
+    val backgroundResId: Int,
+    val title: String,
+    val songInfo: String,
+    val albumImageResId1: Int,
+    val songTitle1: String,
+    val singer1: String,
+    val albumImageResId2: Int,
+    val songTitle2: String,
+    val singer2: String
+)
